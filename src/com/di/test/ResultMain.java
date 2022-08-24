701164e4e3f8eaf58e469bd8322fafebf6142b7a
Miny1110 <wjdalswjd453@naver.com>
2022-08-24 20:54:02 +0900

    0824 복습 주석 추가

--- src/com/di/test/ResultMain.java
+++ src/com/di/test/ResultMain.java
@@ -9,17 +9,26 @@ public class ResultMain {
 
 	public static void main(String[] args) {
 		
+		/**
+		 * path 주소로 가서 그 내용을 읽은 것이 res
+		 * 그냥 그 내용을 단순히 문자화해서 읽기만 한 텍스트 형태로 생각하면 된다.
+		 * 이 읽은 내용을 스프링 언어로 번역해서 컨테이너를 생성하는 과정이 필요
+		 */
 		String path = "com/di/test/applicationContext.xml";
-		
 		Resource res = new ClassPathResource(path);
 		
 		/**
 		 * 스프링의 컨테이너를 생성
-		 * BeanFactory: 외부 자원으로부터 설정 정볼르 읽어와 빈 객체 생성
+		 * BeanFactory: 외부 자원으로부터 설정 정보를 읽어와 빈 객체 생성
+		 * xml파일을 읽어서 그 내용을 문자화시켜서 넣어놓고(res)
+		 * 문자화된 내용을 객체화해서 생성(factory)
 		 */
 		BeanFactory factory = new XmlBeanFactory(res);
 		
-		/**스프링 컨테이너에서 bean객체를 호출*/
+		/**
+		 * 스프링 컨테이너에서 bean객체를 호출
+		 * 객체생성은 자바(스프링)이 했기 때문에 Object일 것. 그래서 downcast 필요
+		 */
 		TestService ob = (TestService)factory.getBean("testService");
 		
 		System.out.println(ob.getValue());
--- src/com/di/test/TestService.java
+++ src/com/di/test/TestService.java
@@ -4,12 +4,14 @@ public class TestService {
 
 	private Test test;
 	
+	/**[기본생성자]*/
 	public TestService() {}
 	
 	/**
 	 * Dependency Injection(DI: 의존성 주입)
 	 * TestService(Test test)로 데이터가 들어와서 test에 데이터가 들어가는 것
-	 * */
+	 * 기본생성자(TestService())로는 보통 하지 않는다.
+	 */
 	
 	/**[오버로딩 생성자로 의존성 주입]*/
 	public TestService(Test test) {
@@ -19,9 +21,9 @@ public class TestService {
 	/**
 	 * [메소드로 의존성 주입]
 	 * 메소드로 의존성 주입 할때에는 기본생성자가 필수다.
-	 * 생성자가 아닌 메소드로 의존석 주입을 할 때에는 무조건 set으로 써주고
-	 * set을 제외한 그 뒤에 쓰는 것은
-	 * application.xml의 property name에 무조건 소문자로 와야한다.
+	 * 메소드는 기본생성자로 객체를 생성한 후에 메소드를 호출하는 것이기 때문
+	 * 생성자가 아닌 메소드로 의존성 주입을 할 때에는 메소드명 앞부분을 무조건 set으로 써주고,
+	 * set을 제외한 그 뒷부분은 application.xml의 property name에 무조건 소문자로 와야한다.
 	 */
 	public void setTest(Test test) {
 		this.test = test;
