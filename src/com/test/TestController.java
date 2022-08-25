74884aa7d80970348fd2a5d204fb0d6756a711b1
Miny1110 <wjdalswjd453@naver.com>
2022-08-25 09:37:26 +0900

    0824 복습 주석 추가

--- src/com/test/TestController.java
+++ src/com/test/TestController.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
+/**컨트롤러의 역할을 하기 위해서 상속을 받아야 한다.
+ * AbstractController : 추상클래스 > 메소드 오버라이드 필요 */
 public class TestController extends AbstractController{
 
 	@Override
@@ -30,10 +32,13 @@ public class TestController extends AbstractController{
 			msg = "퇴근입니다";
 		}
 		
+		/**모델(데이터)*/
 		request.setAttribute("hour", hour);
 		request.setAttribute("message", msg);
 		
-		/** /test/view.jsp랑 같다. */
+		/** 뷰 지정
+		 * /test/view.jsp랑 같다. 
+		 * dispatcher-servlet의 viewResolver가 주소 완성해줌*/
 		return new ModelAndView("test/view");
 	}
 
