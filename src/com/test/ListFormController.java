44e010eed0a9e033cbe320afd38477bc984e09cc
Miny1110 <wjdalswjd453@naver.com>
2022-08-25 20:51:12 +0900

    주석 추가

--- src/com/test/ListFormController.java
+++ src/com/test/ListFormController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
+/** write 창을 띄우는 컨트롤러*/
 public class ListFormController implements Controller{
 
 	@Override
